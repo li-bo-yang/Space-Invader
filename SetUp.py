ee4335e14df0c0fd251d2dd761689f3596675f23
Boyang Li <liboyang@berkeley.edu>
2019-10-06 23:51:23 -0700

    Updated graphics but yet to add missiles to the game

--- SetUp.py
+++ SetUp.py
@@ -64,7 +64,7 @@ def fire_bullet():
 	if bullet_state == "ready":
 		os.system("afplay shoot.wav&")
 		bullet_state = "fire" 
-		bullet.setposition(player.xcor(), player.ycor() + 10)
+		bullet.setposition(player.xcor(), player.ycor() + 15)
 		bullet.showturtle()
 
 #move left and right
@@ -92,12 +92,13 @@ def is_collision(t1, t2):
 	else:
 		return False
 
-#enemy
-num_enemy = 5
-enemies = []
-for _ in range(0, num_enemy):
-	enemies.append(turtle.Turtle())
-for enemy in enemies:
+#enemy stage 1
+stage = 1
+num_enemy1 = 5
+enemies1 = []
+for _ in range(0, num_enemy1):
+	enemies1.append(turtle.Turtle())
+for enemy in enemies1:
 	enemy.color("red")
 	enemy.shape("invader.gif")
 	enemy.penup()
@@ -105,7 +106,24 @@ for enemy in enemies:
 	x = random.randint(-200, 200)
 	y = random.randint(100, 250)
 	enemy.setposition(x, y)
-enemy_speed = 2
+enemy1_speed = 2
+
+"""
+#enemy stage 2
+num_enemy2 = 9
+enemies2 = []
+for _ in range(0, num_enemy2):
+	enemies2.append(turtle.Turtle())
+for enemy in enemies2:
+	enemy.color("red")
+	enemy.shape("invader.gif")
+	enemy.penup()
+	enemy.speed(0)
+	x = random.randint(-200, 200)
+	y = random.randint(100, 250)
+	enemy.setposition(x, y)
+enemy2_speed = 5
+"""
 
 #keyboard binding
 turtle.listen()
@@ -115,15 +133,14 @@ turtle.onkey(fire_bullet, "space")
 
 #main game loop
 while True:
-	#move enemies
-	for enemy in enemies:
-		enemyx = enemy.xcor() + enemy_speed
+	for enemy in enemies1:
+		enemyx = enemy.xcor() + enemy1_speed
 		enemy.setx(enemyx)
 		#enemy reverse direction and down
 		if enemy.xcor() > 280 or enemy.xcor() < -280:
-			for e in enemies:
+			for e in enemies1:
 				e.sety(e.ycor() - 40) 
-			enemy_speed *= -1
+			enemy1_speed *= -1
 		#check for collision between the enemy and bullet
 		if is_collision(bullet, enemy):
 			os.system("afplay explosion.wav&")
@@ -132,12 +149,105 @@ while True:
 			bullet_state = "ready"
 			os.system("afplay reload.wav&")
 			enemy.setposition(random.randint(-200, 200), random.randint(100, 250))
-			enemyx = enemy.xcor() + enemy_speed
+			enemyx = enemy.xcor() + enemy1_speed
 			enemy.setx(enemyx)
 			score += 10
 			score_str = "Score: %s"%score
 			score_pen.clear()
 			score_pen.write(score_str, False, align="left", font=("Arial", 14, "normal"))
+		
+		
+
+		if is_collision(player, enemy):
+			player.hideturtle()
+			enemy.hideturtle()
+			print("Game Over")
+			break
+	
+	#move bullet
+	if bullet_state == "fire":
+		bullet.sety(bullet.ycor() + bullet_speed)
+
+	#bullet check
+	if bullet.ycor() > 280:
+		bullet.hideturtle()
+		if bullet_state != "ready":
+			os.system("afplay reload.wav&")
+		bullet_state = "ready"
+
+#main game loop
+while stage == 1:
+	for enemy in enemies1:
+		enemyx = enemy.xcor() + enemy1_speed
+		enemy.setx(enemyx)
+		#enemy reverse direction and down
+		if enemy.xcor() > 280 or enemy.xcor() < -280:
+			for e in enemies1:
+				e.sety(e.ycor() - 60) 
+			enemy1_speed *= -1
+		#check for collision between the enemy and bullet
+		if is_collision(bullet, enemy):
+			os.system("afplay explosion.wav&")
+			bullet.hideturtle()
+			bullet.setposition(0, -300)
+			bullet_state = "ready"
+			os.system("afplay reload.wav&")
+			enemy.setposition(random.randint(-200, 200), random.randint(100, 250))
+			enemyx = enemy.xcor() + enemy1_speed
+			enemy.setx(enemyx)
+			score += 10
+			score_str = "Score: %s"%score
+			score_pen.clear()
+			score_pen.write(score_str, False, align="left", font=("Arial", 14, "normal"))
+		
+		if score == 100:
+			stage = 2
+
+		if is_collision(player, enemy):
+			player.hideturtle()
+			enemy.hideturtle()
+			print("Game Over")
+			break
+	
+	#move bullet
+	if bullet_state == "fire":
+		bullet.sety(bullet.ycor() + bullet_speed)
+
+	#bullet check
+	if bullet.ycor() > 280:
+		bullet.hideturtle()
+		if bullet_state != "ready":
+			os.system("afplay reload.wav&")
+		bullet_state = "ready"
+
+
+
+while stage == 2:
+	for enemy in enemies2:
+		enemyx = enemy.xcor() + enemy2_speed
+		enemy.setx(enemyx)
+		#enemy reverse direction and down
+		if enemy.xcor() > 280 or enemy.xcor() < -280:
+			for e in enemies2:
+				e.sety(e.ycor() - 60) 
+			enemy2_speed *= -1
+		#check for collision between the enemy and bullet
+		if is_collision(bullet, enemy):
+			os.system("afplay explosion.wav&")
+			bullet.hideturtle()
+			bullet.setposition(0, -300)
+			bullet_state = "ready"
+			os.system("afplay reload.wav&")
+			enemy.setposition(random.randint(-200, 200), random.randint(100, 250))
+			enemyx = enemy.xcor() + enemy2_speed
+			enemy.setx(enemyx)
+			score += 10
+			score_str = "Score: %s"%score
+			score_pen.clear()
+			score_pen.write(score_str, False, align="left", font=("Arial", 14, "normal"))
+		
+		#if score == 100:
+			#stage = 2
 
 		if is_collision(player, enemy):
 			player.hideturtle()
